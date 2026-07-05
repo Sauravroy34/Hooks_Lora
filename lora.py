1f92112cf42e8fd0ac11502924010324f2defe80
Saurav Kumar Roy <136881235+Sauravroy34@users.noreply.github.com>
2026-07-06 02:12:20 +0530

    Swap dimensions of parameters A and B in lora.py

--- lora.py
+++ lora.py
@@ -18,9 +18,8 @@ def apply_lora_to_linear(model, target_layer_name, r=2, a=4):
             for param in module.parameters():
                 param.requires_grad_(False)
 
-            A = nn.Parameter(torch.randn(r, input_size, device=weight_device, dtype=weight_dtype, requires_grad=True))
-            B = nn.Parameter(torch.zeros(output_size, r, device=weight_device, dtype=weight_dtype, requires_grad=True))
-
+            A = nn.Parameter(torch.randn(output_size, r, device=weight_device, dtype=weight_dtype, requires_grad=True))
+            B = nn.Parameter(torch.zeros(r, input_size, device=weight_device, dtype=weight_dtype, requires_grad=True))
             
             module.register_parameter('A', A)
             module.register_parameter('B', B)
